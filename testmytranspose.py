01f5cf5012e7566a1d93073d343a2c28378c5d38
kimvoice <voicepiml@gmail.com>
2025-06-06 21:37:28 +0900

    Feat: Add numpy test scenario by Teammate 1

--- testmytranspose.py
+++ testmytranspose.py
@@ -58,29 +58,7 @@ class TestMyTranspose(unittest.TestCase):
         transposed3 = mytranspose(myvar3)
         self.assertEqual(transposed3.shape, (0, 1))
 
-    # (3) DataFrame의 경우
-    def test_pandas_dataframe(self):
-        D = np.array([1, 2, 3, 4])
-        E = np.array(["red ", " white ", " red ", np.nan])
-        F = np.array([True, True, True, False])
-        df = pd.DataFrame({"d": D, "e": E, "f": F})
-        
-        transposed_df = mytranspose(df)
-        
-        # pandas에서 제공하는 테스트 함수로 비교
-        pd.testing.assert_frame_equal(transposed_df, df.T)
 
-    # (4) PyTorch Tensor의 경우
-    # torch가 설치되어 있지 않으면 이 테스트는 건너뜀
-    @unittest.skipIf(not _torch_available, "PyTorch 라이브러리가 설치되어 있지 않습니다.")
-    def test_pytorch_tensor(self):
-        np_array = np.array([[1, 2], [3, 4]])
-        tensor_pt = torch.tensor(np_array)
-        
-        transposed_tensor = mytranspose(tensor_pt)
-        
-        # torch.equal 함수로 두 텐서가 같은지 비교
-        self.assertTrue(torch.equal(transposed_tensor, tensor_pt.T))
 
 # 이 파일을 직접 실행했을 때 unittest를 실행하도록 하는 코드
 if __name__ == '__main__':
